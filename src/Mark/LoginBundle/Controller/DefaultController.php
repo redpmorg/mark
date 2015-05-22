ac70b46e15db87639f73e9cc976f32b4357e3ef0
redgoat.org <leonardlepadatu@yahoo.com>
2015-05-22 14:52:12 +0300

    ssss

--- src/Mark/LoginBundle/Controller/DefaultController.php
+++ src/Mark/LoginBundle/Controller/DefaultController.php
@@ -3,70 +3,37 @@
 namespace Mark\LoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Security;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\SecurityContext;
 
 
 class DefaultController extends Controller
 {
 	/**
-	 * @Route("/", name="login")
+	 * @Route("/login", name="_login")
 	 * @Template("MarkLoginBundle:Default:login.html.twig")
 	 */
 
-	public function loginAction(Request $request) {
+	public function loginAction() {
 
-		$session = $request->getSession();
+		$authenticationUtils = $this->get('security.authentication_utils');
 
-		if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-		} else {
-			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
-		}
+dump($authenticationUtils->getLastAuthenticationError());
 
-		return array("_username" => $session->get(SecurityContext::LAST_USERNAME), "error" => $error);
+		$lastError = $authenticationUtils->getLastAuthenticationError();
+		$lastUserName = $authenticationUtils->getLastUserName();
+
+		return array("_username" => $lastUserName, "_error" => $lastError);
 	}
 
 	/**
-	 * @Route("/login_check", name="login_check")
-	 * @Template()
+	 * @Route("/login_check", name="_login_check")
+	 * @Template("MarkLoginBundle:Default:login-check.html.twig")
 	 */
 	public function loginCheckAction()
 	{
-
+		return array("_message"=>"This is secured area");
 	}
 }
-
-
-// public function loginAction()
-	// {
-	//     $request = $this->getRequest();
-	//     $session = $request->getSession();
-
-	//     // get the login error if there is one
-	//     if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-	//         $error = $request->attributes->get(
-	//             SecurityContext::AUTHENTICATION_ERROR
-	//         );
-	//     } else {
-	//         $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-	//         $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-	//     }
-
-	//     return $this->render(
-	//         'AcmeSecurityBundle:Security:login.html.twig',
-	//         array(
-	//             // last username entered by the user
-	//             'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-	//             'error'         => $error,
-	//         )
-	//     );
-	// }
-
-	// public function dumpStringAction()
-	// {
-	//   return $this->render('SimpleProfileBundle:Security:dumpString.html.twig', array());
-	// }
